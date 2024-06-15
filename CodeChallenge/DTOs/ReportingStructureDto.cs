09515dffd07fa32181015a47bfbd839e465463f1
Isaiah Bowman <ibowmandev@outlook.com>
2024-06-15 15:10:16 -0400

    Create employee and direct report structure DTOs

---
+++ CodeChallenge/DTOs/ReportingStructureDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeChallenge.Models
+{
+    public class ReportingStructureDto
+    {
+        public EmployeeDto Employee { get; set; }
+        public int NumberOfReports { get; set; }
+    }
+}
